bbab50d3d56ed818a5af4784e06177814d3a29c4
user <user@MacBook-Air-user.local>
2026-02-28 10:25:29 +0300

    deleteCapital

--- src/StringTasks/DetectCapital.java
+++ src/StringTasks/DetectCapital.java
@@ -2,4 +2,35 @@ package StringTasks;
 
 public class DetectCapital {
 
+    public static void main(String[] args) {
+        String a= "adsfsdfasdfasdfasdfasdf";
+        String b = "Qirikki";
+        String c = "A";
+        String d = "bA";
+
+
+        System.out.println(detectCapitalUse(a));
+        System.out.println(detectCapitalUse(b));
+        System.out.println(detectCapitalUse(c));
+        System.out.println(detectCapitalUse(d));
+
+    }
+
+    public static boolean detectCapitalUse(String word) {
+        int n = word.length();
+        String upp = word.toUpperCase();
+        String low = word.toLowerCase();
+        if (word.equals(upp) || word.equals(low)) {
+            return true;
+        }
+        if (word.charAt(0) >= 'A' && word.charAt(0) <= 'Z') {
+            for (int i = 1; i < n; i++) {
+                if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
